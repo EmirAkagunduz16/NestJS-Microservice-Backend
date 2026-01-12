f671fb70246f8326ea246ae51a871d1c6b471f2c
EmirAkagunduz16 <akagunduzemir04@gmail.com>
2026-01-12 20:06:24 +0300

    Add cookie-parser middleware and implement JWT authentication in the Auth module. Enhance UsersController to retrieve current user details with JWT guard. Update package dependencies accordingly.

---
+++ apps/auth/src/strategies/jwt.strategy.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { UsersService } from '../users/users.service';
+import { Request } from 'express';
+import { TokenPayload } from '../interfaces/token-payload.interface';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(
+    configService: ConfigService,
+    private readonly usersService: UsersService,
+  ) {
+    super({
+      jwtFromRequest: ExtractJwt.fromExtractors([
+        (request: Request) => {
+          const cookies = request?.cookies as { Authentication?: string };
+          return cookies?.Authentication ?? null;
+        },
+      ]),
+      secretOrKey: configService.getOrThrow('JWT_SECRET'),
+    });
+  }
+
+  async validate({ userId }: TokenPayload) {
+    return this.usersService.getUser({ _id: userId });
+  }
+}
